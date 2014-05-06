905e908af0cbf13d0c95c106b9d64e24c867d5f8
Parth Shah <parthshahva@gmail.com>
2014-05-06 09:07:10 -0500

    uncomment out clear everything

--- spec/database/shared/database_spec.rb
+++ spec/database/shared/database_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 shared_examples_for "a database" do
   let(:db) { described_class.new }
-  # before { db.clear_everything }
+  before { db.clear_everything }
 
   describe 'Assignments' do
     before do
