b158d1073db4094e881e5a25d780b3162d27934e
Parth Shah <parthshahva@gmail.com>
2014-05-08 16:29:34 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,22 +28,130 @@ student1 = Tyto.db.create_student(username: "Benny Provost",
                               phone_number: "5123333333")
 
 student2 = Tyto.db.create_student(username: "Evie Provost",
-                              password: "password1",
-                              email: "evie@student.com",
-                              phone_number: "5124444444")
+                                  password: "password1",
+                                  email: "evie@student.com",
+                                  phone_number: "5124444444")
 
 student3 = Tyto.db.create_student(username: "Hasmukh Shah",
-                              password: "password1",
-                              email: "hasmukh@student.com",
-                              phone_number: "5125555555")
+                                  password: "password1",
+                                  email: "hasmukh@student.com",
+                                  phone_number: "5125555555")
+course = Tyto.db.create_course(name: "algebra")
 
-student4 = Tyto.db.create_student(username: "Georgia Cross",
-                              password: "password1",
-                              email: "georgia@student.com",
-                              phone_number: "5126666666")
+classroom1 = Tyto.db.create_classroom(teacher_id: teacher1.id,
+                                      course_id:  course.id,
+                                      name: "Period 1" )
+classroom2 = Tyto.db.create_classroom(teacher_id: teacher1.id,
+                                      course_id:  course.id,
+                                      name: "Period 2" )
+classroom3 = Tyto.db.create_classroom(teacher_id: teacher1.id,
+                                      course_id:  course.id,
+                                      name: "Period 3" )
 
-student5 = Tyto.db.create_student(username: "Sandra Bullock Shah",
-                              password: "password1",
-                              email: "sandra@student.com",
-                              phone_number: "5127777777")
+Tyto.db.add_student_to_classroom(classroom_id: classroom1.id,
+                                 student_id: student1.id)
+Tyto.db.add_student_to_classroom(classroom_id: classroom1.id,
+                                 student_id: student2.id)
+Tyto.db.add_student_to_classroom(classroom_id: classroom1.id,
+                                 student_id: student3.id)
+
+Tyto.db.add_student_to_classroom(classroom_id: classroom2.id,
+                                 student_id: student1.id)
+Tyto.db.add_student_to_classroom(classroom_id: classroom2.id,
+                                 student_id: student2.id)
+
+Tyto.db.add_student_to_classroom(classroom_id: classroom3.id,
+                                 student_id: student1.id)
+
+chapter1 = Tyto.db.create_chapter(parent_id: course.id, name: "Chapter One")
+chapter2 = Tyto.db.create_chapter(parent_id: course.id, name: "Chapter Two")
+
+section1 = Tyto.db.create_chapter(parent_id: chapter1.id, name: "1.1")
+section2 = Tyto.db.create_chapter(parent_id: chapter1.id, name: "1.2")
+section3 = Tyto.db.create_chapter(parent_id: chapter1.id, name: "1.3")
+
+section4 = Tyto.db.create_chapter(parent_id: chapter2.id, name: "2.1")
+section5 = Tyto.db.create_chapter(parent_id: chapter2.id, name: "2.2")
+section6 = Tyto.db.create_chapter(parent_id: chapter2.id, name: "2.3")
+
+subtopics = [section1, section2, section3, section1, section2, section3]
+subtopics.each do |x|
+ Tyto.db.create_question(level: 1, question: "1+1",
+                              answer: "2", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+2",
+                              answer: "3", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+3",
+                              answer: "4", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+4",
+                              answer: "5", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+5",
+                              answer: "6", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+6",
+                              answer: "7", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+7",
+                              answer: "8", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+8",
+                              answer: "9", chapter_id: x.id)
+ Tyto.db.create_question(level: 1, question: "1+9",
+                              answer: "10", chapter_id: x.id)
+
+ Tyto.db.create_question(level: 2, question: "2+1",
+                              answer: "3", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+2",
+                              answer: "4", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+3",
+                              answer: "5", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+4",
+                              answer: "6", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+5",
+                              answer: "7", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+6",
+                              answer: "8", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+7",
+                              answer: "9", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+8",
+                              answer: "10", chapter_id: x.id)
+ Tyto.db.create_question(level: 2, question: "2+9",
+                              answer: "11", chapter_id: x.id)
+
+ Tyto.db.create_question(level: 3, question: "3+1",
+                              answer: "4", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+2",
+                              answer: "5", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+3",
+                              answer: "6", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+4",
+                              answer: "7", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+5",
+                              answer: "8", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+6",
+                              answer: "9", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+7",
+                              answer: "10", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+8",
+                              answer: "11", chapter_id: x.id)
+ Tyto.db.create_question(level: 3, question: "3+9",
+                              answer: "12", chapter_id: x.id)
+
+ Tyto.db.create_question(level: 4, question: "4+1",
+                              answer: "5", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+2",
+                              answer: "6", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+3",
+                              answer: "7", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+4",
+                              answer: "8", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+5",
+                              answer: "9", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+6",
+                              answer: "10", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+7",
+                              answer: "11", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+8",
+                              answer: "12", chapter_id: x.id)
+ Tyto.db.create_question(level: 4, question: "4+9",
+                              answer: "13", chapter_id: x.id)
+end
+
+
 
