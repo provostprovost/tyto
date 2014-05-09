94a0d52aa57c7a62e5286476d7cf5b52b9b1515d
Parth Shah <parthshahva@gmail.com>
2014-05-09 08:58:44 -0500

    testing out teacher dashboard

--- web/app/controllers/questions_controller.rb
+++ web/app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
   def show
-    render json: Tyto.db.get_question(params[:question_id])
+   render json: Tyto.db.get_question(params[:id])
   end
 end
