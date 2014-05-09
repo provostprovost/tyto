ffe46ea64bd1e5018cdaa12a47cbc19bb6e5790c
Brian Provost <brianmprovost@gmail.com>
2014-05-09 13:40:35 -0500

    Skeleton for assignment.jsx.js

---
+++ web/app/assets/javascripts/assignments.js.jsx
@@ -0,0 +1,56 @@
+/** @jsx React.DOM */
+var Question = React.createClass({
+  render: function() {
+    // get result.question, render it as new question
+    // return (
+
+    // );
+  }
+});
+
+// proficiency array for D3
+
+var Progress = React.createClass({
+  render: function() {
+    // return (
+
+    // );
+  }
+});
+
+var Streaks = React.createClass({
+  render: function() {
+
+  }
+});
+
+var Assignment = React.createClass({
+  getInitialState: function() {
+    return {  questionText: "",
+              answer: "",
+              questionsAnswered: 0,
+              assignmentSize: 5,
+              currentStreak: 0,
+              longestStreak: 5 };
+  },
+  onChange: function(e) {
+    this.setState({ answer: e.target.value });
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextQuestionText = "";//get next question
+    var nextQuestionsAnswered = 1;//get next questions answered
+    var nextCurrentStreak = 2;//get current streak
+    var nextLongestStreak = 3;//get longest streak
+    this.setState({ questionText: nextQuestionText,
+                    answer: "",
+                    questionsAnswered: nextQuestionsAnswered,
+                    currentStreak: nextCurrentStreak,
+                    longestStreak: nextLongestStreak });
+  },
+  render: function() {
+    // return (
+
+    // );
+  }
+});
