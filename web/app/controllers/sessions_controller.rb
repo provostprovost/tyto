31b603a2064a7905d9909dce82ce8feb3d9a8a62
Brian Provost <brianmprovost@gmail.com>
2014-05-13 10:38:56 -0500

    signin redirects if user is signed in

--- web/app/controllers/sessions_controller.rb
+++ web/app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    if session[:app_session_id]
+      existing_session = Tyto.db.get_session(session[:app_session_id])
+      # binding.pry
+      if existing_session.student_id
+        redirect_to "/students/#{existing_session.student_id}"
+      elsif existing_session.teacher_id
+        redirect_to "/teachers/#{existing_session.teacher_id}"
+      else
+        redirect_to root_url
+      end
+    end
   end
 
   def create
