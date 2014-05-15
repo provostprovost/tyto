92a32d0b0c2109ead62261563e4997ba55e4c6d0
Parth Shah <parthshahva@gmail.com>
2014-05-15 17:16:07 -0500

    adding students and assignments to outputted classroom objects

--- lib/tyto/entities/class.rb
+++ lib/tyto/entities/class.rb
@@ -1,6 +1,6 @@
 module Tyto
   class Classroom < Entity
-    attr_reader :id, :teacher_id, :course_id, :students, :name
+    attr_reader :id, :teacher_id, :course_id, :students, :name, :assignments
     validates_presence_of :teacher_id, :course_id, :name
   end
 end
