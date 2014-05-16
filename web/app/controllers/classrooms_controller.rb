72c304410a57123997d2b22758671d8bbb8821a3
Brian Provost <brianmprovost@gmail.com>
2014-05-16 11:05:08 -0500

    Add index action to classrooms controller

--- web/app/controllers/classrooms_controller.rb
+++ web/app/controllers/classrooms_controller.rb
@@ -1,4 +1,15 @@
 class ClassroomsController < ApplicationController
+  before_action :signed_in_user
+
+  def index
+    current_session = Tyto.db.get_session(session[:app_session_id].to_i)
+    if current_session.student_id
+      @classrooms = Tyto.db.get_classrooms_for_student(current_session.student_id)
+    else
+      @classrooms = Tyto.db.get_classrooms_for_teacher(current_session.teacher_id)
+    end
+    render json: @classrooms
+  end
 
   def create
     params[:teacher_id] = Tyto.db.get_session(session[:app_session_id]).teacher_id
@@ -12,13 +23,15 @@ class ClassroomsController < ApplicationController
     params[:students] = [params[:student_one], params[:student_two]]
     result = Tyto::AddStudentsToClass.run(students_params)
     if result.success?
-        result.invite_ids.each do |x|
-          email = StudentMailer.classroom_invite(x, result.classroom.id)
+        result.invite_ids.each do |invite_id|
+          email = StudentMailer.classroom_invite(invite_id, result.classroom.id)
           email.deliver
         end
     end
     render json: result
   end
+
+  private
 
   def classroom_params
     params.permit(:name,
@@ -32,6 +45,9 @@ class ClassroomsController < ApplicationController
                   :students,
                   :student_one,
                   :student_two)
+  end
 
+  def signed_in_user
+    redirect_to "/signin", notice: "Please sign in." unless !!session[:app_session_id]
   end
 end
