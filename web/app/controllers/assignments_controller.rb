4e330daaa0b4730476ede042f3c53acb7605eddf
Brian Provost <brianmprovost@gmail.com>
2014-05-22 15:55:53 -0500

    Video renders on the page. API call is made in controller if there is a video url associated with a chapter

--- web/app/controllers/assignments_controller.rb
+++ web/app/controllers/assignments_controller.rb
@@ -10,7 +10,12 @@ class AssignmentsController < ApplicationController
   def show
     @assignment = Tyto.db.get_assignment(params[:id])
     respond_to do |format|
-      format.html
+      format.html do
+        embedly_api = Embedly::API.new(:key => ENV['EMBEDLY'],
+          :user_agent => 'Mozilla/5.0 (compatible; tyto/1.0; brianmprovost@gmail.com)')
+        result = embedly_api.extract :url => @assignment.video_url
+        @video_html = result[0]["media"]["html"].html_safe
+      end
       format.json { render json: @assignment }
     end
   end
