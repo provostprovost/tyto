c92cf93bdf2d200117cc0b0aa52a221d7d34c171
Parth Shah <parthshahva@gmail.com>
2014-05-24 09:01:55 -0500

    setting up initials for heroku

---
+++ websockets_server/lib/database.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+class Message
+  include DataMapper::Resource
+  property :id, Serial
+  property :username, String, :required => true
+  property :message, String, :required => true
+  property :classroom_id, Integer
+end
