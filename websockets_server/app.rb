e5d5d9f037475253c11d91ec495073fed5ede85e
Parth Shah <parthshahva@gmail.com>
2014-05-24 09:19:17 -0500

    going back from setting up db here

--- websockets_server/app.rb
+++ websockets_server/app.rb
@@ -1,22 +1,10 @@
 require 'sinatra/base'
 require 'json'
-require_relative 'lib/database.rb'
-
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-  DataMapper.auto_upgrade!
-end
 
 module TytoChat
   class App < Sinatra::Base
     get "/" do
       erb :"index.html"
-    end
-
-    get '/past' do
-    end
-
-    post '/message' do
     end
   end
 end
--- websockets_server/lib/database.rb
+++
@@ -1,9 +0,0 @@
-require 'data_mapper'
-class Message
-  include DataMapper::Resource
-  property :id, Serial
-  property :username, String, :required => true
-  property :message, String, :required => true
-  property :classroom_id, Integer
-end
-DataMapper.finalize
